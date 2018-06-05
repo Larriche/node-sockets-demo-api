cbb45c3b490285d266e518daa7ce4a954afab66c
Richman Larry Clifford <larriche99@gmail.com>
2018-06-05 04:17:48 +0000

    Updates to how messages are loaded

--- src/controllers/messages.js
+++ src/controllers/messages.js
@@ -11,13 +11,26 @@ const messages = {
      * Load and return all messages
      */
     getAll(req, res, next) {
+        let fromId = req.query.user_1 ? req.query.user_1 : null;
+        let toId = req.query.user_2 ? req.query.user_2 : null;
         let userId = req.query.user_id ? req.query.user_id : null;
         let query = {};
 
-        if (userId) {
+        if (fromId && toId) {
             query = {
-                [Op.or]: [{ fromId: userId }, { toId: userId }]
+                [Op.or]: [{
+                    [Op.and]: [{ fromId: fromId }, { toId: toId }]
+                },
+                {
+                    [Op.and]: [{ fromId: toId }, { toId: fromId }]
+                }]
             };
+        } else {
+            if (userId) {
+                query = {
+                    [Op.or]: [{ fromId: userId }, { toId: userId }]
+                }
+            }
         }
 
         query.type = 'message';
